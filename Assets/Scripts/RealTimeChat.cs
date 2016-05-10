d77f7d1102c9c66ca3b397eb75d7b14eb1f846c4
Wix Litariz <wix3000@hotmail.com>
2016-05-10 19:18:31 +0800

    最初版本

---
+++ Assets/Scripts/RealTimeChat.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public static class RealTimeChat {
+
+    public const string FIREBASE_URL = "https://glowing-heat-7000.firebaseio.com";
+    public const string FIREBASE_CHILD = "Message";
+
+    static IFirebase firebase;
+
+    public static Action<FirebaseChangedEventArgs> onDataChange;
+
+    static RealTimeChat() {
+        firebase = Firebase.CreateNew(FIREBASE_URL).Child(FIREBASE_CHILD);
+        firebase.ValueUpdated += OnValueUpdated;
+        firebase.Error += OnError;
+    }
+
+    public static IEnumerator Push(string name, string content) {
+        IFirebase pushed = firebase.Push();
+        pushed.Child("IP").SetValue(Network.player.ipAddress);
+        pushed.Child("name").SetValue(name);
+        pushed.Child("content").SetValue(content);
+        pushed.Child("time").SetValue(DateTime.Now.ToString());
+        yield return null;
+    }
+
+    static void OnValueUpdated(object sender, FirebaseChangedEventArgs e) {
+        if (onDataChange != null) onDataChange(e);
+    }
+
+    private static void OnError(object sender, FirebaseErrorEventArgs e) {
+        Debug.LogError(e.Error);
+    }
+
+    //static void GetChats(Firebase sender, DataSnapshot snapshot) {
+    //    Dictionary<string, object> outdata = snapshot.Value<Dictionary<string, object>>();
+
+    //    foreach (string key in outdata.Keys) {
+    //        chats.Add(new ChatSet(outdata[key] as Dictionary<string, object>));
+    //    }
+    //}
+
+    [UnityEditor.MenuItem("Tool/Test Firebase")]
+    public static void TestFirebase() {
+        Dictionary<string, object> dir = new Dictionary<string, object>();
+        dir.Add("Test", "TTTest");
+        firebase.Push().SetValue(dir as IDictionary<string, object>);
+    }
+
+}
---
+++ Assets/Scripts/SampleScene.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SampleScene : MonoBehaviour {
+
+    [SerializeField]
+    InputField nameField, contentField;
+    [SerializeField]
+    Text showText;
+
+    Dictionary<string, object> snapshots = new Dictionary<string, object>();
+
+	// Use this for initialization
+	void Start () {
+        RealTimeChat.onDataChange += OnDataChange;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(snapshots != null) {
+            Refresh();
+            snapshots = null;
+        }
+	}
+
+    /// <summary>
+    /// 重新整理
+    /// </summary>
+    void Refresh() {
+
+        StringBuilder sb = new StringBuilder();
+        List<string> keys = new List<string>(snapshots.Keys);
+        keys.Sort((x, y) => (snapshots[x] as Dictionary<string, object>)["time"].ToString().CompareTo(
+                            (snapshots[y] as Dictionary<string, object>)["time"].ToString()));
+
+        for (int i = 0; i < keys.Count; i++) {
+            Dictionary<string, object> chat = snapshots[keys[i]] as Dictionary<string, object>;
+            sb.Append((chat["IP"].ToString() == Network.player.ipAddress) ? "您" : chat["name"]);
+            sb.Append(" : ");
+            sb.Append(chat["content"]);
+            sb.Append("  (" + chat["time"] + ")\n");
+        }
+
+        showText.text = sb.ToString();
+    }
+
+    void OnDataChange(FirebaseChangedEventArgs e) {
+        snapshots = e.DataSnapshot.DictionaryValue;
+    }
+
+    /// <summary>
+    /// 按下發送時的事件
+    /// </summary>
+    public void OnSendClick() {
+        if (nameField == null || contentField == null) return;
+
+        StartCoroutine(RealTimeChat.Push(nameField.text, contentField.text));
+        contentField.text = "";
+    }
+
+    /// <summary>
+    /// 檢查姓名是否為空
+    /// </summary>
+    public void OnNameEndEdit() {
+        if(nameField.text == "") {
+            nameField.text = "不具名";
+        }
+    }
+}
